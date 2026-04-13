be717439c0b95f4d1b5c5a9ce17b718dfef9aefd
David Cappelletto <cappellettodavid@gmail.com>
2026-04-13 11:16:16 +0200

    aggiunta sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://davidcappelletto.it",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}

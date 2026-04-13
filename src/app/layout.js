0a821da6df73941559a608ca53f0925887c9d7dc
David Cappelletto <cappellettodavid@gmail.com>
2026-04-13 11:37:40 +0200

    aggiunto Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "David Cappelletto | Consulenza Digitale",
@@ -13,6 +14,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="it" className="h-full">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MBWPX09G1C"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MBWPX09G1C');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col antialiased">
         {children}
       </body>
